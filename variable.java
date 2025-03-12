08935bdaeffa5532179a0af14eb19d5b2660ad62
paramu5054 <parameshwari2777>
2025-03-12 15:09:48 +0530

    sathya fall for him

---
+++ variable.java
@@ -0,0 +1,11 @@
+public class variable
+{
+    public static void main(String []args)
+    {
+        int a=8;
+        int b=9;
+        int c=a+b;
+        System.out.println(c);
+        //hello
+    }
+}
