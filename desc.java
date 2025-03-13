be46e2b29add272f42106d9f10860c14153c8031
paramu5054 <parameshwari2777>
2025-03-13 10:18:13 +0530

    good

---
+++ desc.java
@@ -0,0 +1,22 @@
+public class desc 
+{
+    public static void main(String [] args)
+    {
+        String Name="parameshwari";
+        String Class="cse b";
+        int a1=76;
+        int a2=98;
+        int a3=89;
+        char Grade='a';
+        System.out.println("Name:"+Name);
+        System.out.println("Class:"+Class);
+        System.out.println("mark 1:"+a1);
+        System.out.println("mark 2:"+a2);
+        System.out.println("mark 3:"+a3);
+        System.out.println("Grade:"+Grade);
+
+
+
+    }
+    
+}
