9c9cb2682f8a1edf2a9bcbdbfe2bd0536c7d4203
paramu5054 <parameshwari2777>
2025-03-13 11:54:41 +0530

    i am good

---
+++ comparision.java
@@ -0,0 +1,18 @@
+public class comparision
+{
+    public static void main(String [] args)
+    {
+       String str1="paramu";
+       String str2="paramesh";
+       if(str1==str2)
+       {
+
+        System.out.println("true");
+       }
+       else
+       {
+        System.out.println("False");
+       }
+        
+    }
+}
