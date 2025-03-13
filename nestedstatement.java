92a12f21323e6b8d431a3d08a1fc0430c509427c
paramu5054 <parameshwari2777>
2025-03-13 15:28:41 +0530

    sathya is feeling sad

---
+++ nestedstatement.java
@@ -0,0 +1,20 @@
+public class nestedstatement
+{
+    public static void main(String [] args)
+    {
+        int mark=90;
+        System.out.println("Enter the mark:"+mark);
+        if(mark<=100)
+        {
+            System.out.println("Grade:O");
+        }
+        else if(mark<=90)
+        {
+            System.out.println("Grade:A");
+        }
+        else if(mark<=80)
+        {
+            System.out.println("Grade:B");
+        }
+    }
+}
