3fc152aadff2dca639f14b7c5ce5afbbc2a41149
paramu5054 <parameshwari2777>
2025-03-13 12:01:58 +0530

    i am sad

---
+++ comparision2.java
@@ -0,0 +1,16 @@
+public class comparision2 
+{
+    public static void  main(String [] args)
+    {
+    Integer int1=6;
+    Integer int2=8;
+    if(int1==int2)
+    {
+        System.out.println("True");
+    }
+    else
+    {
+        System.out.println("False");
+    }
+}
+}
