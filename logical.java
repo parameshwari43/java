78f03dede265ce7a1276ab71e011614e9c9a98fb
paramu5054 <parameshwari2777>
2025-03-13 13:24:13 +0530

    i am very happy

---
+++ logical.java
@@ -0,0 +1,22 @@
+public class logical
+{
+    public static void main(String [] args)
+    {
+        int mark1=89;
+        int mark2=78;
+        if(mark1==mark2 && mark1>mark2)
+        {
+          System.out.println("Grade:a");
+        }
+        if(mark2==mark1 || mark2>mark1)
+        {
+            System.out.println("Grade:b");
+        } 
+        else
+        {
+            System.out.println("invalid");
+        }       
+    }
+ }
+
+
